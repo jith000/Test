50dc2e876445ef41258ed9a130fb2d45622812ba
admin <admin@admin-PC>
2018-08-15 22:12:58 +0530

    new poject

---
+++ saireddy/src/saireddy/Inherit.java
@@ -0,0 +1,17 @@
+package saireddy;
+
+public class Inherit extends Inheritence{
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is a inheritence logic");
+		
+		Inherit i = new Inherit();
+		i.add();
+		i.Multi();
+			
+		}
+
+	}
+
+
---
+++ saireddy/src/saireddy/Inheritence.java
@@ -0,0 +1,20 @@
+package saireddy;
+
+public class Inheritence {
+
+	public static void main(String[] args)
+	    {
+		Inheritence inh = new Inheritence();
+		inh.add();
+		inh.Multi();
+		System.out.println("This is saireddy");
+        }
+	public void add()
+    {
+    	System.out.println("This is addition");
+    }
+    public void Multi()
+    {
+    	System.out.println("This is multip");
+    }
+}
